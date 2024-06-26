056761d49f2d3556aa42a648e75b24023383588e
Estevanzc <134285029+Estevanzc@users.noreply.github.com>
2024-06-26 16:34:33 -0300

    Update data_requester.js

--- data_requester.js
+++ data_requester.js
@@ -1,5 +1,5 @@
 async function data_requester(action) {
-    var data = await fetch(`http://localhost/vs_code/catalogo-cheiroso/data_taker.php?action=${action}`)
+    var data = await fetch(`http://localhost/catalogo-cheiroso/data_taker.php?action=${action}`)
     data = await data.json()
     console.log(data)
 }
