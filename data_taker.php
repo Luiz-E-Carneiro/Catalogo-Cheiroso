5d253d248301968d09775137a53887e01d00d5e8
Estevanzc <esteveplays@gmail.com>
2024-06-30 14:55:16 -0300

    backend (acho que ta pronto)

--- data_taker.php
+++ data_taker.php
@@ -4,10 +4,10 @@ $client = new \GuzzleHttp\Client();
 session_start();
 function data_requester($link, $client) {
     $data = $client->request('GET', "https://api.themoviedb.org/3/$link", [
-    'headers' => [
-        'Authorization' => 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxOWFkZTEwNjNlM2EwZGYyNmQ5NzgwYTZlYjA5NzQ4MCIsIm5iZiI6MTcxOTQyMzQwMy4yMjMxMzIsInN1YiI6IjY2N2FhMjZjYTUwYmFmNTEwMDE1OTY5ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LjJbl4DxxSw-r6n9uwBohQmjxK0DLth0Hkxx_TBKqJs',
-        'accept' => 'application/json',
-    ],
+        'headers' => [
+            'Authorization' => 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxOWFkZTEwNjNlM2EwZGYyNmQ5NzgwYTZlYjA5NzQ4MCIsIm5iZiI6MTcxOTQyMzQwMy4yMjMxMzIsInN1YiI6IjY2N2FhMjZjYTUwYmFmNTEwMDE1OTY5ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LjJbl4DxxSw-r6n9uwBohQmjxK0DLth0Hkxx_TBKqJs',
+            'accept' => 'application/json',
+        ],
     ]);
     $data = json_decode($data->getBody());
     $data = $data->results;
@@ -22,7 +22,6 @@ if (isset($_GET["item"])) {
 }
 $type = (int) isset($_GET["type"]) ? $_GET["type"] : 0;
 $require_awnser = [];
-$link;
 switch ($action) {
     case 0:
         $require_awnser = [
@@ -44,7 +43,7 @@ switch ($action) {
         break;
     case 5:
         if (isset($_SESSION["data"]["fav"][$type])) {
-            $require_awnser = json_decode($_SESSION["data"]["fav"][$type]);
+            $require_awnser = $_SESSION["data"]["fav"][$type];
         } else {
             $require_awnser = [
                 "err" => "Empty State",
@@ -54,7 +53,7 @@ switch ($action) {
         break;
     case 6:
         if (isset($_SESSION["data"]["watch_list"][$type])) {
-            $require_awnser = json_decode($_SESSION["data"]["watch_list"][$type]);
+            $require_awnser = $_SESSION["data"]["watch_list"][$type];
         } else {
             $require_awnser = [
                 "err" => "Empty State",
@@ -63,14 +62,40 @@ switch ($action) {
         }
         break;
     case 7:
-        $_SESSION["data"]["fav"][$type] = [json_encode($item), $item->id];
+        $_SESSION["data"]["fav"][$type][] = [$item, $item->id];
+        $require_awnser = $_SESSION["data"]["fav"][$type];
         break;
     case 8:
-        $_SESSION["data"]["watch_list"][$type] = [json_encode($item), $item->id];
+        $_SESSION["data"]["watch_list"][$type][] = [$item, $item->id];
+        $require_awnser = $_SESSION["data"]["watch_list"][$type];
         break;
     case 9:
-        foreach ($_SESSION["data"]["fav"][$type] as $data) {
-            $data = json_decode($data);
+        if (count($_SESSION["data"]["fav"][$type]) >= 1) {
+            $counter = 0;
+            foreach ($_SESSION["data"]["fav"][$type] as $data) {
+                if ($data[1] == $item) {
+                    unset($_SESSION["data"]["fav"][$type][$counter]);
+                    $_SESSION["data"]["fav"][$type] = array_values($_SESSION["data"]["fav"][$type]);
+                    break;
+                }
+                $counter++;
+            }
         }
+        $require_awnser = $_SESSION["data"]["fav"][$type];
+        break;
+    case 10:
+        if (count($_SESSION["data"]["watch_list"][$type]) >= 1) {
+            $counter = 0;
+            foreach ($_SESSION["data"]["watch_list"][$type] as $data) {
+                if ($data[1] == $item) {
+                    unset($_SESSION["data"]["watch_list"][$type][$counter]);
+                    $_SESSION["data"]["watch_list"][$type] = array_values($_SESSION["data"]["watch_list"][$type]);
+                    break;
+                }
+                $counter++;
+            }
+        }
+        $require_awnser = $_SESSION["data"]["watch_list"][$type];
+        break;
 }
 print_r(json_encode($require_awnser));
