2f85241b418d7df92682002881e70b57113fe778
Estevanzc <134285029+Estevanzc@users.noreply.github.com>
2024-07-02 08:43:50 -0300

    frontend

--- interactions.js
+++ interactions.js
@@ -9,7 +9,19 @@ var cards_area = document.getElementById("cards-area")
 var load_screen = document.getElementById("load_screen")
 var empty_screen = document.getElementById("empty_screen")
 var data
+var action_num
 async function data_requester(action, item = null, type = null) {
+    var counter
+    counter = Number(cards_area.children.length)
+    document.querySelectorAll('.unity').forEach(function(element) {
+        element.remove();
+    });
+    /*if (counter >= 3) {
+        for (var i = 2; i <= counter-1; i ++) {
+            console.log(cards_area.children);
+            cards_area.removeChild(cards_area.children[i])
+        }
+    }*/
     console.log(`http://localhost/catalogo-cheiroso/data_taker.php?action=${action}&item=${item}&type=${type}`)
     data = await fetch(`http://localhost/catalogo-cheiroso/data_taker.php?action=${action}&item=${item}&type=${type}`)
     data = await data.json()
@@ -26,7 +38,7 @@ function content_require(element) {
     load_screen.style.display = "flex"
     element.classList.add("clicked")
     element.parentNode.children[element.classList[0] == "btn-filme" ? 1 : 0].classList.remove("clicked")
-    var action_num = Number(element.dataset.action_num)
+    action_num = Number(element.dataset.action_num)
     var action_item
     var action_type
     if (action_num >= 5) {
@@ -47,18 +59,35 @@ function content_require(element) {
 function content_creater() {
     data.forEach(function(element, i) {
         var unity = document.createElement("div")
+        unity.addEventListener("click", function() {
+            show_modal(this)
+        })
+        unity.addEventListener("mouseenter", function() {
+            show_desc(this)
+        })
+        unity.addEventListener("mouseleave", function() {
+            unshow_desc(this)
+        })
         unity.classList.add("unity")
         unity.dataset.data_num = i
         unity.dataset.content_id = element.id
-        unity.style.backgroundImage = `url("https:/${element.backdrop_path}")`
+        unity.style.backgroundImage = `url("http://image.tmdb.org/t/p/w500${element.backdrop_path}")`
         var unity_desc = document.createElement("div")
         unity_desc.classList.add("unity_desc")
         var div = document.createElement("div")
         var p0 = document.createElement("p")
         var p1 = document.createElement("p")
         var p2 = document.createElement("p")
-        p0.innerHTML = element.title
-        p1.innerHTML = element.release_date.substring(0, 4)
+        if (element.title != undefined) {
+            p0.innerHTML = element.title
+        } else {
+            p0.innerHTML = element.name
+        }
+        if (element.release_date != undefined) {
+            p1.innerHTML = element.release_date.substring(0, 4)
+        } else {
+            p1.innerHTML = element.first_air_date.substring(0, 4)
+        }
         p2.innerHTML = `${Number(element.vote_average).toFixed(1)}/10`
         div.appendChild(p1)
         div.appendChild(p2)
@@ -70,5 +99,17 @@ function content_creater() {
             unity.style.opacity = 1
         }, (1000 * (i / 100)));
     });
+}
+var unity_time_out
+function show_desc(element) {
+    clearTimeout(unity_time_out)
+    element.children[0].style.display = "flex"
+    element.children[0].style.opacity = 1
+}
+function unshow_desc(element) {
+    element.children[0].style.opacity = 0
+    unity_time_out = setTimeout(() => {
+        element.children[0].style.display = "none"
+    }, 250);
 }
 content_require(document.getElementsByClassName("btn-filme")[0])
